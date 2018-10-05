589f454dec6e3198216f1ac44d3c6ec14bc87b47
Pattesz1998 <43583841+Pattesz1998@users.noreply.github.com>
2018-10-05 22:33:49 +0200

    Add files via upload

---
+++ labdapattogtatas.cpp
@@ -0,0 +1,36 @@
+#include "unistd.h"
+#include "std_lib_facilities.h"
+
+
+#define SZEL 78
+#define MAG 22
+
+int putX(x,y)
+{
+{
+	int i;
+
+	for(i=0;i<x;i++)
+		cout<< ("\n");
+
+	for(i=0;i<y;i++)
+		cout<< (" ");
+
+	cout<<("X\n");
+return 0;
+}
+
+int main()
+{
+
+int x=0,y=0;
+
+while(1)
+{
+system("clear");
+putX(abs(MAG-(x++%(MAG*2))),abs(SZEL-(y++%(SZEL*2))));
+usleep(50000);
+}
+
+return 0;
+}
